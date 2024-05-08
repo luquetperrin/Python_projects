f3dc4a181def20663bfdc918d5bff526ecd7c111
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-05-09 00:27:02 +0200

    test

---
+++ seeing_the_world.py
@@ -0,0 +1,11 @@
+locattions = ["South Africa", "Ivory Cost", "Zanzibar", "Canada", "Italy"]
+
+print("Here is the original list:")
+print(locattions)
+
+sorted_locations = sorted(locattions)
+print("\nHere is the sorted list:")
+print("sorted_locations")
+
+print("\nHere is the original list again:")
+print(locattions)
