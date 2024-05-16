097c7e9823f3abef5c9b0ea6a398338f0e40ab0f
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-05-16 17:11:14 +0200

    Filling a Dictionary with User Input

--- new programs/loop_with_lists.py
+++ new programs/loop_with_lists.py
@@ -18,3 +18,13 @@ confirmed_users = []
 # Verifiy each user until are no more unconfirmed users.
 # Move each verified user into the list of confirmed users.
 
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+
+# Display all confirmed users.
+print("\nThe following have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
